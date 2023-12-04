584f08ad2e6ad3abe40f31f3972527650a97db10
tiedyefreak22 <olivine8910@gmail.com>
2023-12-03 17:49:27 -0700

    Adding CSV creator and starting loss graphing

--- start_tf_jupyter.cmd
+++ start_tf_jupyter.cmd
@@ -1,6 +1,6 @@
 @echo off
 cd\
-cd C:\Users\khard\Documents\GitHub\jupyter
+cd C:\Users\khard\OneDrive\Documents\GitHub\GEORGE
 call mamba activate tensorflow
 call jupyter lab --NotebookApp.iopub_data_rate_limit=1.0e10
 ::PAUSE
